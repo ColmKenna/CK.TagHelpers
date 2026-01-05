f4c78cc00b20108ac51f0692ebe9340f7d041e51
ColmKenna <43966678+ColmKenna@users.noreply.github.com>
2026-01-05 16:44:01 +0000

    Add project files.

---
+++ CK.Taghelpers.SampleUsage/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CK.Taghelpers.SampleUsage.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
